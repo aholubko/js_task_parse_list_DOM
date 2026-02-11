0845623a0ce42b435a39e279836add4b4461ee96
Anastasiia Holubko <nastya12092006@gmail.com>
2026-02-11 22:05:56 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
 // write code here
+const list = document.querySelector('ul');
+
+function getSalary(li) {
+  const salaryString = li.dataset.salary;
+  const withoutDollar = salaryString.replace('$', '');
+  const cleanString = withoutDollar.replace(/,/g, '');
+
+  return Number(cleanString);
+}
+
+function sortList(ul) {
+  const items = Array.from(ul.querySelectorAll('li'));
+
+  items.sort((a, b) => {
+    const salaryA = getSalary(a);
+    const salaryB = getSalary(b);
+
+    return salaryB - salaryA;
+  });
+
+  ul.innerHTML = '';
+
+  for (const item of items) {
+    ul.append(item);
+  }
+}
+
+function getEmployees(ul) {
+  const items = Array.from(ul.querySelectorAll('li'));
+
+  return items.map((li) => {
+    return {
+      name: li.textContent.trim(),
+      position: li.dataset.position,
+      salary: getSalary(li),
+      age: Number(li.dataset.age),
+    };
+  });
+}
+
+sortList(list);
+getEmployees(list);
